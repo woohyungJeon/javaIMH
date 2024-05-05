1c703538c682b41c29870010f1309df320997207
woohyungJeon <97295764+woohyungJeon@users.noreply.github.com>
2024-05-05 21:12:39 +0900

    no message

--- src/CCCCC/DDD.java
+++ src/CCCCC/DDD.java
@@ -8,8 +8,8 @@ public class DDD extends AAA{
         //멤버 사용해보기
         System.out.println(a);
         System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
+//        System.out.println(c);
+//        System.out.println(d);
     }
 
 }
---
+++ src/CollectionTest/CollectionBasicTest.java
@@ -0,0 +1,16 @@
+package CollectionTest;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+public class CollectionBasicTest {
+    List<Integer> number1 = new ArrayList<Integer>();
+    List<Integer> number2 = new Vector<Integer>();
+    List<Integer> number3 = new LinkedList<Integer>();
+
+    ArrayList<Integer> number4 = new ArrayList<Integer>();
+    Vector<Integer> number5 = new Vector<Integer>();
+    LinkedList<Integer> number6 = new LinkedList<Integer>();
+}
